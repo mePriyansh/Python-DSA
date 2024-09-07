efd697396aa28e6f03766ffe7cf27006a33b40ad
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-07 23:48:02 +0530

    2d array search

--- Arrays/Two d array search.py
+++ Arrays/Two d array search.py
@@ -3,12 +3,15 @@ import numpy as np
 two_d_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(two_d_array)
 
-def Traversal(array):
+def searchArray(array, target):
     for i in range(len(array)):                                     #O(m*n)
         for j in range(len(array[i])):                              #O(n)                         
-            print(array[i][j])
-            
-Traversal(two_d_array)            
+            if array[i][j] == target:
+                return "Target found at index: " + str(i) + ", " + str(j)
+    return "Target not found"
+
+print(searchArray(two_d_array, 5))
+print(searchArray(two_d_array, 11))
 
 #Time complexity: O(n^2)/O(m*n)
 #Space complexity: O(1)
