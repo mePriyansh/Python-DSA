44129fc99abea41e48adeedc3fa185016e833d15
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-07 23:39:06 +0530

    2d array traversal

--- Arrays/Two d array traversal.py
+++ Arrays/Two d array traversal.py
@@ -4,8 +4,11 @@ two_d_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(two_d_array)
 
 def Traversal(array):
-    for i in range(len(array)):
-        for j in range(len(array[i])):
+    for i in range(len(array)):                                     #O(m*n)
+        for j in range(len(array[i])):                              #O(n)                         
             print(array[i][j])
             
 Traversal(two_d_array)            
+
+#Time complexity: O(n^2)
+#Space complexity: O(1)
