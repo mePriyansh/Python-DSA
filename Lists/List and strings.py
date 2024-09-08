83d085ea025f60cdeb6b68e4a0dcaab7a0477fab
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-09 00:00:10 +0530

    List operation and functions

---
+++ Lists/List and strings.py
@@ -0,0 +1,18 @@
+new='hello world'
+#Converting a string into a list
+n_list=list(new)
+print(n_list)
+
+#Splitting a string into a list
+n_list1=new.split()
+print(n_list1)
+
+#Splitting a string into a list with a delimiter
+new1='hello,world'
+n_list2=new.split(',')
+print(n_list2)
+
+#Joining a list into a string
+n_list3=['how','are','you']
+new2=' '.join(n_list3)
+print(new2)
