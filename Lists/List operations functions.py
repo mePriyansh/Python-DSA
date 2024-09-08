e17209004fb64980cd43b627345fdf4a27b8490d
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-08 23:29:27 +0530

    List operation and functions

---
+++ Lists/List operations functions.py
@@ -0,0 +1,38 @@
+#List Operations
+
+#1. List Concatenation
+print("List Concatenation")
+a=[1,2,3,4,5]
+b=[6,7,8,9,10]
+c=a+b
+print(c)
+
+#2. List Repetition
+print("\nList Repetition")
+d=[1,2]
+e=d*3
+print(e)
+
+#List Functions
+
+n_list=[10,20,30,40,50]
+
+#1. len()
+print("\nLength of List")
+print(len(n_list))
+
+#2. max()
+print("\nMaximum value in List")
+print(max(n_list))
+
+#3. min()
+print("\nMinimum value in List")
+print(min(n_list))
+
+#4. sum()
+print("\nSum of List")
+print(sum(n_list))
+
+#5. average
+print("\nAverage of List")
+print(sum(n_list)/len(n_list))
