8ceae70e9c177f2bc58623c16fe517afc922e410
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-09 22:57:12 +0530

    Days above Average temperature

--- Average temp and days above.py
+++ Average temp and days above.py
@@ -1,8 +1,18 @@
 numDays=int(input("Enter the number of days: "))
 total=0
-for i in range(1,numDays+1):
-    nextDay=float(input("Enter the temperature for day "+str(i)+": "))
-    total+=nextDay
+temp=[]
+
+for i in range(numDays):
+    nextDay=float(input("Enter the temperature for day "+str(i+1)+": "))
+    temp.append(nextDay)
+    total+=temp[i]
     
 avg= round(total/numDays,2)
 print("The average temperature is: ",str(avg))
+
+above=0
+for i in temp:
+    if i>avg:
+        above+=1
+        
+print("The number of days above average is: ",str(above))
