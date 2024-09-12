4b3450800da0f0a0f54fca8212841f9e9f45aa2b
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-12 17:31:28 +0530

    2D list diagonal sum

---
+++ Array and List Prac/2D lists.py
@@ -0,0 +1,23 @@
+'''
+Given 2D list calculate the sum of diagonal elements.
+
+Example
+
+myList2D= [[1,2,3],[4,5,6],[7,8,9]] 
+ 
+diagonal_sum(myList2D)
+'''
+def diagonal_sum(matrix):
+    # Initialize the sum to 0
+    total = 0
+ 
+    # Iterate through the rows of the matrix
+    for i in range(len(matrix)):
+        # Add the diagonal element to the total sum
+        total += matrix[i][i]
+ 
+    return total
+
+myList2D= [[1,2,3],[4,5,6],[7,8,9]] 
+ 
+print(diagonal_sum(myList2D))
