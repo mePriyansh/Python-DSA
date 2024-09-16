46007ae1650640046c1316b4f0e870f59a6e6244
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-16 22:59:32 +0530

    Dictionary insert and update

---
+++ Dictionaries/Dictionary insert update.py
@@ -0,0 +1,9 @@
+#Inserting key-value pairs in a dictionary
+mydict = {'name':'Jack', 'age': 26}
+mydict['address'] = 'Downtown'
+print(mydict)
+
+#Updating a key-value pair in a dictionary
+mydict1 = {'name':'Jack', 'age': 26}
+mydict1['age'] = 27
+print(mydict1)
