2b77207508b0c9b4d3da5250e21be5c821380849
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-18 00:23:56 +0530

    common keys

---
+++ Dictionaries Practice/commonKeys.py
@@ -0,0 +1,37 @@
+'''
+Define a function with takes two dictionaries as parameters and merge them and sum the values of common keys.
+
+Example:
+
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'b': 3, 'c': 4, 'd': 5}
+merge_dicts(dict1, dict2)
+Output:
+
+{'a': 1, 'b': 5, 'c': 7, 'd': 5}
+'''
+
+
+def merge_dicts(dict1, dict2):
+    result = dict1.copy()
+    for key, value in dict2.items():
+        result[key] = result.get(key, 0) + value
+    return result
+
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'b': 3, 'c': 4, 'd': 5}
+print(merge_dicts(dict1, dict2))
+
+#Other way
+def merge_dicts(dict1, dict2):
+    merged_dict = dict1.copy()  # Make a copy of dict1 to preserve original data
+    for key, value in dict2.items():
+        if key in merged_dict:
+            merged_dict[key] += value
+        else:
+            merged_dict[key] = value
+    return merged_dict
+
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'b': 3, 'c': 4, 'd': 5}
+print(merge_dicts(dict1, dict2))
