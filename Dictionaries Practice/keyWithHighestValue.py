33e5792602bd2e4423dc1146a9e84808660fc0e5
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-21 09:34:21 +0530

    key with highest value

--- Dictionaries Practice/keyWithHighestValue.py
+++ Dictionaries Practice/keyWithHighestValue.py
@@ -29,3 +29,6 @@ def max_value_key(my_dict):
     return max(my_dict, key=my_dict.get)
 my_dict = {'a': 5, 'b': 9, 'c': 2}
 print(max_value_key((my_dict)))
+
+#Time complexity: O(n)
+#Space complexity: O(1)
