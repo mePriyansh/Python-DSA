5a106ccc98eb3380ea8f99cd9c4e4ccd21f444dc
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-21 14:49:24 +0530

    Tuple basic

---
+++ Tuples/tuple.py
@@ -0,0 +1,10 @@
+newTuple = ('a', 'b', 'c', 'd', 'e')
+print(newTuple)
+
+# Tuple with tuple() constructor
+newTuple1=tuple('abcde')
+print(newTuple1)
+
+#Tuple with single element
+newTuple2=('a',)
+print(newTuple2)
