e296ece11052971152ca41792fd123a8e56b3aa7
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-22 10:04:26 +0530

    Sum Product

---
+++ Tuple prac/sumProduct.py
@@ -0,0 +1,14 @@
+'''
+Write a function that calculates the sum and product of all elements in a tuple of numbers.
+'''
+
+def sum_product(input_tuple):
+    sum_result=0
+    product_result=1
+    for i in input_tuple:
+        sum_result+=i
+        product_result*=i
+    return sum_result,product_result        
+input_tuple = (1, 2, 3, 4)
+sum_result, product_result = sum_product(input_tuple)
+print(sum_result, product_result)
