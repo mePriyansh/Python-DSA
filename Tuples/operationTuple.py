c9324885a607f1176aaa365929af3c6a57e088f0
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-22 10:02:33 +0530

    Operation on Tuple

---
+++ Tuples/operationTuple.py
@@ -0,0 +1,12 @@
+tuple1=(1,4,3,2,5)
+tuple2=(1,2,6,9,8,7)
+
+# Concatenation of two tuples
+print("Concatenation of two tuples")
+newtuple=tuple1+tuple2
+print(newtuple)
+
+# Repetition of tuple
+print("Repetition of tuple")
+new_tuple1=tuple1*2
+print(new_tuple1)
