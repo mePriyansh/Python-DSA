968c9b0cc9d5b73e57dfab8f872a2334f72207f9
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-26 11:50:31 +0530

    Class and Object default value

--- Classes and Objects/test.py
+++ Classes and Objects/test.py
@@ -1,8 +1,8 @@
 class Cookie:
-    def __init__(self, color, shape):
+    def __init__(self, color, shape='star'):
         self.color = color
         self.shape = shape
 
-cookie1 = Cookie("blue", "circle") 
+cookie1 = Cookie("blue") 
 print(cookie1.color)
 print(cookie1.shape)
