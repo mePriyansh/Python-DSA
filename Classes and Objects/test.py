a30df2dd31466493d84f70981c8d9d32a22690fc
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-26 11:50:07 +0530

    Class and Object

---
+++ Classes and Objects/test.py
@@ -0,0 +1,8 @@
+class Cookie:
+    def __init__(self, color, shape):
+        self.color = color
+        self.shape = shape
+
+cookie1 = Cookie("blue", "circle") 
+print(cookie1.color)
+print(cookie1.shape)
