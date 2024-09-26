325689285c0329f2fe4f8ac0daea0a20f967bb65
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-09-26 14:20:42 +0530

    Class Methods

--- Classes and Objects/test.py
+++ Classes and Objects/test.py
@@ -1,8 +1,17 @@
 class Cookie:
+#class attributes
     def __init__(self, color, shape='star'):
         self.color = color
         self.shape = shape
 
+#class methods
+    def __del__(self):
+        print("A cookie has been eaten")
+
 cookie1 = Cookie("blue") 
 print(cookie1.color)
 print(cookie1.shape)
+cookie2 = Cookie("red", "circle")
+print(cookie2.color)
+print(cookie2.shape)
+del cookie1
