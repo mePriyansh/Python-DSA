78a55a1073c1c137cb57b59fe09341bed0d520db
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-10-09 15:22:53 +0530

    Insertion at beginning

--- Linked List/Insert at end.py
+++ Linked List/Insert at end.py
@@ -18,7 +18,8 @@ class LinkedList:
                 result+='->'
             temp_node=temp_node.next
         return result
-        
+
+#Insertion at the end of the linked list
     def append(self,value):
         new_node = Node(value)
         if self.head == None:
@@ -28,13 +29,27 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
+#Time complexity: O(1)
+#Space complexity: O(1)
+        
+#Insertion at the beginning of the linked list
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+#Time complexity: O(1)
+#Space complexity: O(1)
         
         
 new_linked_list = LinkedList()
 print("Initial Length: ",new_linked_list.length)
 new_linked_list.append(10)
 new_linked_list.append(20)
-print("Head value of new node: ",new_linked_list.head.value)
-print("Tail value of new node: ",new_linked_list.tail.value)
-print("Length after adding two nodes: ",new_linked_list.length)
-print("New Linked List:",new_linked_list)
+print("Linked list after appending",new_linked_list)
+new_linked_list.prepend(5)
+print("Linked list after prepending",new_linked_list)
