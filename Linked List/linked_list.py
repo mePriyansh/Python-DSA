043d6310f1d11ba07d90bebba92e658307bd6527
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-10-09 11:04:18 +0530

    Linked List class

--- Linked List/linked_list.py
+++ Linked List/linked_list.py
@@ -2,10 +2,31 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        
-new_node = Node(10)
-print(new_node)
 
+#Time Complexity: O(1)
+#Space Complexity: O(1)
+
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+        
+new_linked_list = LinkedList(10)
+print(new_linked_list.head.value)
+
+#Time Complexity: O(1)
+#Space Complexity: O(1)
+        
+#Empty Linked List
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
+#         self.length = 0
+# ll=LinkedList()
+# print(ll.length)        
 
 #Time Complexity: O(1)
 #Space Complexity: O(1)
