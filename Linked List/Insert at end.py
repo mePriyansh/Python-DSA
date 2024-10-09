747b7fce1cfb45d12cec707dc62c7f1a8dbfd8af
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-10-09 12:48:05 +0530

    __str__ method added

--- Linked List/Insert at end.py
+++ Linked List/Insert at end.py
@@ -9,6 +9,16 @@ class LinkedList:
         self.tail = None
         self.length = 0
         
+    def __str__(self):
+        temp_node=self.head
+        result=' '
+        while temp_node is not None:
+            result+=str(temp_node.value)
+            if temp_node.next is not None:
+                result+='->'
+            temp_node=temp_node.next
+        return result
+        
     def append(self,value):
         new_node = Node(value)
         if self.head == None:
@@ -19,6 +29,7 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         
+        
 new_linked_list = LinkedList()
 print("Initial Length: ",new_linked_list.length)
 new_linked_list.append(10)
@@ -26,3 +37,4 @@ new_linked_list.append(20)
 print("Head value of new node: ",new_linked_list.head.value)
 print("Tail value of new node: ",new_linked_list.tail.value)
 print("Length after adding two nodes: ",new_linked_list.length)
+print("New Linked List:",new_linked_list)
