68abc510d25338793328563cf98d65b31396884a
Priyansh Bhandari <priyanshb1234@gmail.com>
2024-11-17 17:59:24 +0530

    Prpending the value

--- Circular Singly Linked List/Prepend.py
+++ Circular Singly Linked List/Prepend.py
@@ -47,6 +47,8 @@ class CSLinkedList:
             self.head=new_node
             self.tail.next=self.head
         self.length+=1
+    #Time complexity: O(1)
+    #Space complexity: O(1)
         
 cslinkedlist=CSLinkedList()
 cslinkedlist.prepend(10)
